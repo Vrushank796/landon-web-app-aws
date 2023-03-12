ed1ca83069e1e338a273ada503404f2ae8abdd45
Vrushank796 <42446203+Vrushank796@users.noreply.github.com>
2023-03-12 14:16:57 -0400

    Converted components to use dynamic json data

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,31 +1,14 @@
 import React from 'react';
+import photoGalleryData from './data/photo_gallery.json';
 
 const Welcome = () => {
   return (
     <div className='scene' id='welcome'>
       <article className='content'>
         <div className='gallery'>
-          <img
-            src='https://landonhotel.com/images/hotel/intro_room.jpg'
-            alt='Intro Gallery Room Sample Pictures'
-          />
-          <img
-            src='https://landonhotel.com/images/hotel/intro_pool.jpg'
-            alt='Intro Gallery Pool Sample Pictures'
-          />
-          <img
-            src='https://landonhotel.com/images/hotel/intro_dining.jpg'
-            alt='Intro Gallery Dining Sample Pictures'
-          />
-          <img
-            src='https://landonhotel.com/images/hotel/intro_attractions.jpg'
-            alt='Intro Gallery Attractions Sample Pictures'
-          />
-          <img
-            className='hidesm'
-            src='https://landonhotel.com/images/hotel/intro_wedding.jpg'
-            alt='Intro Gallery Dining Sample Pictures'
-          />
+          {photoGalleryData.map((image) => (
+            <img src={image.src} alt={image.alt} className={image.className} />
+          ))}
         </div>
         <h1>Welcome to the Landon&nbsp;Hotel</h1>
         <p>
